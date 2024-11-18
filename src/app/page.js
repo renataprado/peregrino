10d0ddba0fd3c7ce0515d4d0edc544ea41aaee6e
renaskigi <dev.renataprado@gmail.com>
2024-11-17 21:23:51 -0300

    selected place draft

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,27 @@
-'use client';
+"use client";
 
-import {useState} from "react";
+import { useState } from "react";
+
+import { PrimeReactProvider, PrimeReactContext } from "primereact/api";
+
 import PasswordPage from "./pages/PasswordPage";
 export default function Home() {
   const [currentPage, setCurrentPage] = useState();
 
-
   return (
-    <div style={{ height: "100vh", width: "100vw" }}>
-      <main style={{display: "flex", "justify-content": "center"}}>
-        {/* { currentPage === 'next' && <NextHome />}
+    <PrimeReactProvider>
+      <div style={{ height: "100vh", width: "100vw" }}>
+        <main style={{ display: "flex", "justify-content": "center" }}>
+          {/* { currentPage === 'next' && <NextHome />}
         <button onClick={() => setCurrentPage('next')}>
           Next Home
         </button> */}
-        {/* <PinnedMap /> */}
-        <PasswordPage />
-      </main>
-    </div>
+          {/* <PinnedMap /> */}
+          <PasswordPage />
+        </main>
+      </div>
+    </PrimeReactProvider>
   );
-}5
-
+}
 
 //AIzaSyAXIDFceYnPXUyPs4xjT3Omh4Ws8X7hV3E
--- src/app/pinned-map/page.js
+++ src/app/pinned-map/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
   APIProvider,
   Map,
@@ -8,14 +8,22 @@ import {
   AdvancedMarker,
   Pin,
 } from "@vis.gl/react-google-maps";
+import { Button } from "primereact/button";
+
+import { ListBox } from "primereact/listbox";
 
 const key = "AIzaSyAXIDFceYnPXUyPs4xjT3Omh4Ws8X7hV3E";
 
 export const PinnedMap = () => {
+  const [selectedPlace, setSelectedPlace] = useState();
+  
   return (
-    <div style={{ width: "600px", height: "600px"}}>
+    <div style={{ width: "100wv", height: "100vh" }}>
       Map
-      <MapLoader />
+      <div style={{ display: "flex" }}>
+        <MapLoader places={places} selectedPlace={selectedPlace}/>
+        <PlaceList places={places} selectedPlace={selectedPlace} setSelectedPlace={setSelectedPlace}/>
+      </div>
     </div>
   );
 };
@@ -23,50 +31,88 @@ const places = [
   {
     position: { lat: -23.58374, lng: -46.57464 },
     text: "Primeiro date",
-    desc: "👩‍❤️‍👨"
+    desc: "👩‍❤️‍👨",
   },
   {
-    position: { lat:  -23.58869, lng: -46.57548 },
+    position: { lat: -23.58869, lng: -46.57548 },
     text: "Segundo date Matrona",
-    desc: " "
+    desc: " ",
   },
   {
     position: { lat: -23.59611, lng: -46.60297 },
     text: "Date chique",
     desc: "🍝",
-    color: "#f2e494"
+    color: "#f2e494",
   },
   {
     position: { lat: -23.59317, lng: -46.61412 },
     text: "Aquário! 🐻‍❄️",
     desc: "🐻‍❄️",
-    color: "#77b0e6"
+    color: "#77b0e6",
   },
 ];
 
-const MapLoader = () => {
+const PlaceList = ({ places, selectedPlace, setSelectedPlace }) => {
+
+  const placeTemplate = (option) => {
+    return (
+      <div className="flex align-items-center">
+        <div>{option.text}</div>
+      </div>
+    );
+  };
+  return (
+    <>
+      <div className="placeList__container">
+        <ListBox
+          value={selectedPlace}
+          onChange={(e) => setSelectedPlace(e.value)}
+          options={places}
+          optionLabel="text"
+          className="w-full md:w-14rem"
+          itemTemplate={placeTemplate}
+        />
+      </div>
+
+      {/* <div className="placeList__container">
+        { places.map((p) => (
+          <div>{p.text}</div>
+        ))}
+      </div> */}
+    </>
+  );
+};
+
+const MapLoader = ({places, selectedPlace}) => {
+
+  const renderPlaces = () => {
+    if(selectedPlace) return [selectedPlace]
+    return places
+  }
+
   return (
     <APIProvider apiKey={key}>
       <Map
         mapId={"peregrino-map"}
-        style={{ width: "100%", height: "100%" }}
+        style={{ width: "500px", height: "500px" }}
         defaultCenter={{ lat: -23.58825, lng: -46.603831 }}
         defaultZoom={13}
         gestureHandling={"greedy"}
         disableDefaultUI={true}
       >
-        {places.map(({ position, label, text, desc, color }, i) => (
+        {renderPlaces().map(({ position, label, text, desc, color }, i) => (
           <AdvancedMarker
             key={i}
-            position={{...position}}
+            position={{ ...position }}
             title={label}
             clickable={true}
             onClick={() => alert(text)}
           >
-            <Pin 
-              background={color ? color : "#fcc5f7"} 
-              borderColor = {"#636363"} 
-              scale={1.4}>
+            <Pin
+              background={color ? color : "#fcc5f7"}
+              borderColor={"#636363"}
+              scale={1.4}
+            >
               {desc}
             </Pin>
           </AdvancedMarker>
